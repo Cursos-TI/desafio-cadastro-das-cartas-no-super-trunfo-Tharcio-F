22c469db3538e18b2189684d799a541bf57ca58b
Tharcio-F <228211757+Tharcio-F@users.noreply.github.com>
2025-08-27 20:24:30 +0000

    commit new

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,6 +8,7 @@
 
 int main() {
     printf("desafio do super trunfo\n");
+    printf("novo comit\n");
 
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
